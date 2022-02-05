ef32e451b895f3e9baffa04d79573530699a4e15
wizardofcroz <connocrosby62@gmail.com>
2022-02-05 11:21:57 -0500

    change client output

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ api_key = secrets.api_key
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print('Logged on as {0}!'.format(self.activity))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
