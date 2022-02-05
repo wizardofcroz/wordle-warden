5d6ecd0f309a41f3503980ad6f20e747d6879a9f
wizardofcroz <connocrosby62@gmail.com>
2022-02-05 13:09:52 -0500

    send message if form wordle

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+from operator import truediv
 import discord
 import secrets
 import logging
-api_key = secrets.api_key
+# Worlde channel = 939204794051100712
 
+api_key = secrets.api_key
 client = discord.Client()
+
 logging.getLogger().setLevel(logging.INFO)
+
+def fromWordleChannel(message):
+    if (message.channel.id == 939204794051100712):
+        return True
+    else:
+        return False
+
 @client.event
 async def on_ready():
     print('Logged on as {0}!'.format(client))
     for c in client.get_all_channels():
         logging.info('Name: %s\n' \
                       'ID: %s\n',c.name,c.id)
-    # async def on_message(self, message):
-    #     print('Message from {0.author}: {0.content}'.format(message))
 
+@client.event
+async def on_message(message):
+    if(fromWordleChannel(message)):
+       await message.channel.send("hello")
 # @client.event
 # async def on_message(message):
 #     if message.author == client.user:
