de2b85772314dc31e94075bd47e99351a946ae15
wizardofcroz <connocrosby62@gmail.com>
2022-02-05 12:16:23 -0500

    change warn to info

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import logging
 api_key = secrets.api_key
 
 client = discord.Client()
-
+logging.getLogger().setLevel(logging.INFO)
 @client.event
 async def on_ready():
     print('Logged on as {0}!'.format(client))
     for c in client.get_all_channels():
-        logging.warning('Name: %s\n \
-                      ID: %s\n',c.name,c.id)
+        logging.info('Name: %s\n' \
+                      'ID: %s\n',c.name,c.id)
     # async def on_message(self, message):
     #     print('Message from {0.author}: {0.content}'.format(message))
 
