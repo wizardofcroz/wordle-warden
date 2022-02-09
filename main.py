615f558e224322c957b46e416085515efb0157d9
harryg30 <gordenstein30@gmail.com>
2022-02-09 15:23:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ async def on_message(message):
 
 @tasks.loop(minutes=1)
 async def send_message():
+    await client.get_channel('939204794051100712').send(helper.getWinners())
     if(datetime.now().time().hour == 20 and datetime.now().time().minute == 30):
         await client.get_channel('939204794051100712').send(helper.getWinners())
     if(datetime.now().time().hour == 15 and datetime.now().time().minute == 22):
