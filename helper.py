e9cfa19b0b803e7e3ed3f1b551b783067c8f5b98
harryg30 <gordenstein30@gmail.com>
2022-02-09 15:57:46 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -73,7 +73,6 @@ def getWinners():
 
     winners = ''
     for p in players.items():
-        print(p)
         if p[1] == topScore:
             winners = winners +"\n`"+ p[0] + "`"
     file1.close()
