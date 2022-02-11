cd67b50de63b28916a1fc72f57bfb2e0dffee2a4
James Desmond <jamesdesmond13@gmail.com>
2022-02-11 14:35:05 -0500

    changes from closed PRs

--- helper.py
+++ helper.py
@@ -1,83 +1,31 @@
 from datetime import date
 from datetime import time
 import datetime
+import textwrap
+import csv
 
 
-def toFiveCharLine_(text):
-    fiveCharline = ''
-    for i in range(0, len(text)):
-        if (i % 5 != 0):
-            if text[i] == ' ':
-                fiveCharline = fiveCharline + "\n"
-            else:
-                fiveCharline = fiveCharline + text[i]
-        else:
-            fiveCharline = fiveCharline + text[i] + "\n"
-
-    return fiveCharline
+def to_five_char_line(text):
+    return textwrap.wrap(text, width=5)
 
 
-def toFiveCharLine(text):
-    fiveCharline = ''
-    splitList = str(text).split(' ')
-    for word in splitList:
-        newWord = ''
-        for i in range(0, len(word)):
-            if (i % 5 != 0 or i == 0):
-                newWord = newWord + word[i]
-            else:
-                newWord = newWord + word[i] + "\n"
-        fiveCharline = fiveCharline + newWord + "\n"
-    return fiveCharline
+def is_five_letters(text):
+    return all(len(line) <= 5 for line in str(text).splitlines())
 
 
-def isFiveLetters(text):
-    isGood = True
-    checkList = str(text).split('\n')
-
-    for line in checkList:
-        if len(line) > 5:
-            isGood = False
-    return isGood
-
-
-'''param message is a Wordle score of the format 
-   'Wordle 231 6/6\n ⬛🟩🟨⬛⬛\n 🟩🟩⬛🟨⬛\n 🟩🟩🟩⬛⬛\n 🟩🟩🟩⬛⬛\n 🟩🟩🟩⬛⬛\n 🟩🟩🟩🟩🟩'
-'''
-
-
-def recordScore(message):
-    today = date.today()
+def record_score(message):
     user = message.author.nick
     score = message.content.split(" ")[2][0]
+    with open(f'{date.today().strftime("%d-%m-%Y")}.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writeline([user, score])
+    return score
 
-    file1 = open(today.strftime("%d-%m-%Y")+'.txt', 'a')
-    #file1 = open(today.strftime("%d-%m-%Y") +'.txt', 'a' )
-    file1.write(user+score+"\n")
-    file1.close()
 
-
-def getWinners():
-    today = date.today()
-    file1 = open(today.strftime("%d-%m-%Y") + '.txt', 'r')
-
-    scoresString = str(file1.read())
-    scores = scoresString.split("\n")
-
-    players = {}
-    topScore = 10
-    for s in scores:
-        try:
-            players.update({s[:-1]: int(s[-1])})
-            if(int(s[-1]) < topScore):
-                topScore = int(s[-1])
-        except:
-            nothing = True
-
-    winners = ''
-    for p in players.items():
-        if p[1] == topScore:
-            winners = winners + "\n`" + p[0] + "`"
-    file1.close()
-
-    return ("Today's winners are " + winners + "\nwith a score of "+str(topScore))
+def get_winners():
+    with open(f'{date.today().strftime("%d-%m-%Y")}.csv', 'r') as file:
+        reader = csv.reader(file)
+        scores = [(x[0], x[1]) for x in reader]  # List of tuples in form (name, score)
+        topScore = max(x[1] for x in scores)  # Get maximum score value from list of scores
+        winners = set([x[0] for x in scores if x[1] == topScore])  # List of names with score == topScore
+        return """Today's winners are {0}\n with a score of: {1}""".format("\n".join(winners), str(topScore))
